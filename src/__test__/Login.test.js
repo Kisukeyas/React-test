f6ab3d30ca063f46af956034ef2a6c032e23e00d
Kisukeyas <kisuke.yas@gmail.com>
2022-09-14 00:08:35 +0900

    2022/09/14

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import Login from "../Login";
+import userEvent from "@testing-library/user-event";
+import Login, { validateEmail } from "../Login";
 
 describe("Test Login Component", () => {
     test("render form with 1 button", async () => {
@@ -8,4 +9,36 @@ describe("Test Login Component", () => {
         // console.log(buttonList)
         expect(buttonList).toHaveLength(1);
     });
+
+    test("should be failed on email validation", () => {
+        const testEmail = "shincode.com";
+        expect(validateEmail(testEmail)).not.toBe(true);
+    })
+    
+    test("should be successed on email validation", () => {
+        const testEmail = "shincode@gmail.com";
+        expect(validateEmail(testEmail)).toBe(true);
+    })
+
+    test("password input should have type password", async () => {
+        render(<Login />);
+        const password = await screen.getByPlaceholderText("パスワード入力");
+        expect(password).toHaveAttribute("type", "password");
+    })
+
+    test("should be able to submit the form", () => {
+        render(<Login />);
+        const submitButton = screen.getByTestId("submit");
+        const email = screen.getByPlaceholderText("メールアドレス入力");
+        const password = screen.getByPlaceholderText("パスワード入力");
+        
+
+        userEvent.type(email, "shincode@gmail.com");
+        userEvent.type(password, "abcdef");
+
+        userEvent.click(submitButton);
+        const userInfo = screen.getByText("shincode@gmail.com");
+
+        expect(userInfo).toBeInTheDocument();
+    })
 });
